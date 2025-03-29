5cb67c03b1ea12b35dabe1af41c6d8b5959baff3
dinesh kumar <140300098+dineshkumars29@users.noreply.github.com>
2025-03-29 20:02:21 +0530

    isolate and particle shape added

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: BubblesAnimation(
+            particleCount: 100,
+            particleColor: Colors.amberAccent,
+            particleRadius: 25.0,
+            particleSpeed: 2.0,
+            shape: ParticleShape.circle,
+            backgroundColor: Colors.blueAccent.withOpacity(0.5),
             widget: Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -46,13 +52,14 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Padding(
               padding: const EdgeInsets.all(15.0),
               child: Container(
+                height: 45,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(20.0),
                   ),
                   child: Padding(
                       padding:
-                          const EdgeInsets.only(left: 15, right: 15, top: 5),
+                          const EdgeInsets.only(left: 15, right: 15),
                       child: TextFormField(
                           decoration: const InputDecoration(
                         border: InputBorder.none,
@@ -64,13 +71,14 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Padding(
               padding: const EdgeInsets.all(15.0),
               child: Container(
+                  height: 45,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(20.0),
                   ),
                   child: Padding(
                       padding:
-                          const EdgeInsets.only(left: 15, right: 15, top: 5),
+                          const EdgeInsets.only(left: 15, right: 15),
                       child: TextFormField(
                           decoration: const InputDecoration(
                         border: InputBorder.none,
@@ -78,22 +86,23 @@ class _MyHomePageState extends State<MyHomePage> {
                       )))),
             ),
           ),
+          const SizedBox(height: 30,),
           Padding(
-              padding: const EdgeInsets.all(50),
+              padding: const EdgeInsets.all(15),
               child: SizedBox(
-                height: 50,
+                height: 45,
                 width: double.infinity,
                 child: ElevatedButton(
                   onPressed: () {},
                   style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.amberAccent,
+                      backgroundColor: Colors.white,
                       // padding:
                       //     EdgeInsets.symmetric(horizontal: 50, vertical: 20),
                       textStyle: const TextStyle(
                           fontSize: 20, fontWeight: FontWeight.bold)),
                   child: const Text(
                     'Sign in',
-                    style: TextStyle(color: Colors.black, fontSize: 18.0),
+                    style: TextStyle(color: Colors.black, fontSize: 16.0),
                   ),
                 ),
               )),
