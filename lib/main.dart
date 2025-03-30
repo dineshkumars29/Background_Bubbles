471e7765bd4ce204fb69956b4e45c3da733aec57
dinesh kumar <140300098+dineshkumars29@users.noreply.github.com>
2025-03-30 21:59:24 +0530

    update web support

--- lib/main.dart
+++ lib/main.dart
@@ -44,70 +44,73 @@ class _MyHomePageState extends State<MyHomePage> {
             shape: ParticleShape.circle,
             backgroundColor: Colors.blueAccent.withOpacity(0.5),
             widget: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Container(
-                height: 45,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Container(
+                          height: 45,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 15, right: 15),
+                              child: TextFormField(
+                                  decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                labelText: 'Email',
+                              )))),
+                    ),
                   ),
-                  child: Padding(
-                      padding:
-                          const EdgeInsets.only(left: 15, right: 15),
-                      child: TextFormField(
-                          decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        labelText: 'Email',
-                      )))),
-            ),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Container(
-                  height: 45,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20.0),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Container(
+                          height: 45,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 15, right: 15),
+                              child: TextFormField(
+                                  decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                labelText: 'Password',
+                              )))),
+                    ),
                   ),
-                  child: Padding(
-                      padding:
-                          const EdgeInsets.only(left: 15, right: 15),
-                      child: TextFormField(
-                          decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        labelText: 'Password',
-                      )))),
-            ),
-          ),
-          const SizedBox(height: 30,),
-          Padding(
-              padding: const EdgeInsets.all(15),
-              child: SizedBox(
-                height: 45,
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      // padding:
-                      //     EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                      textStyle: const TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold)),
-                  child: const Text(
-                    'Sign in',
-                    style: TextStyle(color: Colors.black, fontSize: 16.0),
+                  const SizedBox(
+                    height: 30,
                   ),
-                ),
-              )),
-        ],
-      ),
-    )));
+                  Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: SizedBox(
+                        height: 45,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              // padding:
+                              //     EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                              textStyle: const TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold)),
+                          child: const Text(
+                            'Sign in',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 16.0),
+                          ),
+                        ),
+                      )),
+                ],
+              ),
+            )));
   }
 }
